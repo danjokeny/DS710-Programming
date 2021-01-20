50713fc5d6884c72fa58286ae7d34b3c8a06e78f
Danny Forero <36682157+danjokeny@users.noreply.github.com>
2021-01-20 10:19:25 -0600

    initial upload 1/20/2021
    here is the initial upload of assignement 1 for DS710

---
+++ assignment1.py
@@ -0,0 +1,37 @@
+"""
+###   ds 710: Programming for Data Science
+###   Assignment 1
+
+###   Your Name Here: Daniel (Danny) Forero
+
+###   Do not rename this file
+###"""
+
+### RUNNING CODE ###
+
+# Put your cursor in the following line and press f9 to run it.
+print("hello world")
+
+
+#Note that you may also run a selected line of code by pressing the
+#"Run selection or current line" button in the toolbar.
+
+### QUICK PYTHON PREVIEW ###
+## VARIABLES ##
+
+#create a variable named a, and store the value 3+1 in it
+a = 3+1
+
+#display a
+print(a)
+
+### f-string formatting
+first_name = "Danny" 
+last_name = "Forero"
+print(first_name,last_name)
+
+### Alignment using tabs
+print(f"Given Name\tSurname\n{first_name}\t\t{last_name}")
+
+### String concatenation
+print(first_name + " " + last_name)
